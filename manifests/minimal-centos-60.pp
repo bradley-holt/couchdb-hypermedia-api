25f7d16dfdc25a3668e0a2a8a927ae74a38bf980
Bradley Holt <bradley.holt@foundline.com>
2012-10-04 12:32:28 -0400

    Install epel-release and ius-release packages.

--- manifests/minimal-centos-60.pp
+++ manifests/minimal-centos-60.pp
@@ -33,6 +33,19 @@ class minimal-centos-60 {
     ],
   }
 
+  package { "epel-release":
+    provider => "rpm",
+    ensure => "installed",
+    source => "http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/epel-release-6-5.noarch.rpm"
+  }
+
+  package { "ius-release":
+    require => Package["epel-release"],
+    provider => "rpm",
+    ensure => "installed",
+    source => "http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/ius-release-1.0-10.ius.el6.noarch.rpm"
+  }
+
 }
 
 include minimal-centos-60
