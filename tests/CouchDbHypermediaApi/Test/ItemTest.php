a9e1634837f669ba06f2d2d71b5e26863cf07bac
Bradley Holt <bradley.holt@foundline.com>
2012-10-06 22:16:10 -0400

    Test POST to collection URI, GET item URI, PUT to item URI, and DELETE item URI.

---
+++ tests/CouchDbHypermediaApi/Test/ItemTest.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace CouchDbHypermediaApi\Test;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Doctrine\CouchDB\CouchDBClient;
+use Doctrine\CouchDB\View\FolderDesignDocument;
+use Guzzle\Service\Client;
+
+class ItemTest extends TestCase
+{
+    const ROOT_URI = 'http://localhost:5984/{dbname}/_design/app/_rewrite/';
+
+    const INITIAL_URI = '/';
+
+    /**
+     * @var string
+     */
+    private $testDbName;
+
+    /**
+     * @var Doctrine\CouchDB\CouchDBClient
+     */
+    private $couchDbClient;
+
+    public function setUp()
+    {
+        $this->testDbName = uniqid('test_');
+        $this->couchDbClient = CouchDBClient::create(array(
+            'dbname'    => $this->testDbName,
+        ));
+        $this->couchDbClient->createDatabase($this->testDbName);
+        $dir = new \DirectoryIterator(APPLICATION_ROOT . '/_design');
+        foreach ($dir as $file) {
+            if ($file->isDir() && !$file->isDot()) {
+                $designDoc = new FolderDesignDocument($file->getPathname());
+                $this->couchDbClient->createDesignDocument($file->getFilename(), $designDoc);
+            }
+        }
+    }
+
+    public function tearDown()
+    {
+        $this->couchDbClient->deleteDatabase($this->testDbName);
+    }
+
+    /**
+     * @depends CouchDbHypermediaApi\Test\CollectionTest::testPostToInitialUri
+     */
+    public function testPostToCollectionUri()
+    {
+        $client = new Client(self::ROOT_URI, array(
+        	'dbname' => $this->testDbName,
+        ));
+        $postedCollectionResource = new \stdClass();
+        $postedCollectionResource->title = 'Sessions';
+        $request = $client->post(ltrim(self::INITIAL_URI, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedCollectionResource));
+        $response = $request->send();
+        $collectionResource = json_decode((string) $response->getBody());
+        $postedItemResource = new \stdClass();
+        $postedItemResource->title = 'Building a Hypermedia API in CouchDB';
+        $request = $client->post(ltrim($collectionResource->_links->self->href, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedItemResource));
+        $response = $request->send();
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertNotNull((string) $response->getHeader('Location'));
+        $this->assertEquals('application/hal+json', (string) $response->getHeader('Content-Type'));
+        $this->assertEquals('Accept', (string) $response->getHeader('Vary'));
+        $itemResource = json_decode((string) $response->getBody());
+        $this->assertNotNull($itemResource->_links->self->href);
+        $this->assertEquals($collectionResource->_links->self->href, $itemResource->_links->collection->href);
+        $this->assertEquals($postedItemResource->title, $itemResource->title);
+    }
+
+    /**
+     * @depends testPostToCollectionUri
+     */
+    public function testGetItemUri()
+    {
+        $client = new Client(self::ROOT_URI, array(
+        	'dbname' => $this->testDbName,
+        ));
+        $postedCollectionResource = new \stdClass();
+        $postedCollectionResource->title = 'Sessions';
+        $request = $client->post(ltrim(self::INITIAL_URI, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedCollectionResource));
+        $response = $request->send();
+        $collectionResource = json_decode((string) $response->getBody());
+        $postedItemResource = new \stdClass();
+        $postedItemResource->title = 'Building a Hypermedia API in CouchDB';
+        $request = $client->post(ltrim($collectionResource->_links->self->href, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedItemResource));
+        $response = $request->send();
+        $itemResource = json_decode((string) $response->getBody());
+        $request = $client->get(ltrim($itemResource->_links->self->href, '/'), array(
+            'Accept'        => 'application/hal+json',
+        ));
+        $response = $request->send();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/hal+json', (string) $response->getHeader('Content-Type'));
+        $this->assertEquals('Accept', (string) $response->getHeader('Vary'));
+        $resource = json_decode((string) $response->getBody());
+        $this->assertEquals($itemResource->_links->self->href, $resource->_links->self->href);
+        $this->assertEquals($collectionResource->_links->self->href, $resource->_links->collection->href);
+        $this->assertEquals($postedItemResource->title, $resource->title);
+    }
+
+    /**
+     * @depends testPostToCollectionUri
+     */
+    public function testPutToItemUri()
+    {
+        $client = new Client(self::ROOT_URI, array(
+        	'dbname' => $this->testDbName,
+        ));
+        $postedCollectionResource = new \stdClass();
+        $postedCollectionResource->title = 'Sessions';
+        $request = $client->post(ltrim(self::INITIAL_URI, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedCollectionResource));
+        $response = $request->send();
+        $collectionResource = json_decode((string) $response->getBody());
+        $postedItemResource = new \stdClass();
+        $postedItemResource->title = 'Building a Hypermedia API in CouchDB';
+        $request = $client->post(ltrim($collectionResource->_links->self->href, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedItemResource));
+        $response = $request->send();
+        $itemResource = json_decode((string) $response->getBody());
+        $itemResource->description = <<<'EOD'
+Unlike relational databases, document databases like CouchDB and Couchbase do not directly support entity 
+relationships. This talk will explore patterns of modeling one-to-many and many-to-many entity relationships in a 
+document database. These patterns include using an embedded JSON array, relating documents using identifiers, using a 
+list of keys, and using relationship documents.
+EOD;
+        $request = $client->put(ltrim($itemResource->_links->self->href, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($itemResource));
+        $response = $request->send();
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertEquals('application/hal+json', (string) $response->getHeader('Content-Type'));
+        $this->assertEquals('Accept', (string) $response->getHeader('Vary'));
+        $resource = json_decode((string) $response->getBody());
+        $this->assertEquals($itemResource->_links->self->href, $resource->_links->self->href);
+        $this->assertEquals($collectionResource->_links->self->href, $resource->_links->collection->href);
+        $this->assertEquals($itemResource->title, $resource->title);
+        $this->assertEquals($itemResource->description, $resource->description);
+    }
+
+    /**
+     * @depends testPostToCollectionUri
+     */
+    public function testDeleteItemUri()
+    {
+        $client = new Client(self::ROOT_URI, array(
+        	'dbname' => $this->testDbName,
+        ));
+        $postedCollectionResource = new \stdClass();
+        $postedCollectionResource->title = 'Sessions';
+        $request = $client->post(ltrim(self::INITIAL_URI, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedCollectionResource));
+        $response = $request->send();
+        $collectionResource = json_decode((string) $response->getBody());
+        $postedItemResource = new \stdClass();
+        $postedItemResource->title = 'Building a Hypermedia API in CouchDB';
+        $request = $client->post(ltrim($collectionResource->_links->self->href, '/'), array(
+            'Content-Type'  => 'application/hal+json',
+            'Accept'        => 'application/hal+json',
+        ), json_encode($postedItemResource));
+        $response = $request->send();
+        $deleteResource = json_decode((string) $response->getBody());
+        $request = $client->delete(ltrim($deleteResource->_links->self->href, '/'), array(
+            'Accept'        => 'application/hal+json',
+        ));
+        $deleteResponse = $request->send();
+        $this->assertEquals(201, $deleteResponse->getStatusCode());
+        $this->assertNull($deleteResponse->getHeader('Location'));
+        $this->assertEquals('application/hal+json', (string) $deleteResponse->getHeader('Content-Type'));
+        $this->assertEquals('Accept', (string) $deleteResponse->getHeader('Vary'));
+        $resource = json_decode((string) $deleteResponse->getBody());
+        $this->assertTrue($resource->ok);
+        $request = $client->get(ltrim($deleteResource->_links->self->href, '/'), array(
+            'Accept'        => 'application/hal+json',
+        ));
+        $this->setExpectedException('\Guzzle\Http\Exception\ClientErrorResponseException');
+        $request->send();
+    }
+}
